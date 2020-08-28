89e052de206ad73cc20bba4740858112f15fdfab
Pretisy <Pretisy@gmail.com>
2020-08-28 15:17:54 -0400

    Updated Bot Token
    Updated Bot token to allow Hiromi-Chan to be Online.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require(`discord.js`);
 const bot = new Discord.Client();
 
-const token = "NDY1MjcyODk3ODUwODM0OTQ1.XbTWiA.vQ8zGHAx7iiEnQoNdTfmqgDe9Mc";
+const token = "NDY1MjcyODk3ODUwODM0OTQ1.W0E1Jw.KwEaAsWogvgAB9ltR8DDGdH5Gq0";
 
 bot.on("ready", () => {
   console.log("This bot is Online.");
